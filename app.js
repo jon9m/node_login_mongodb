7f6826e1b4c260f68714a70013863b0d10115a4a
jon9m <manojam79@gmail.com>
2017-12-07 18:39:01 +1100

    NodeJS login module with mongodb initial

---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:loginapp');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+//Init app
+var app = express();
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));                    //views folder
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));      //default view layout.handlebars
+app.set('view engine', 'handlebars');
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));   //stylesheet, imgs, JQuery etc ... publicly accsessible to the browser
+
+//Express session middleware
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//Connect flash middleware
+app.use(flash());
+
+//Global vars
+
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+    console.log("Server created on port " + app.get('port'));
+});
---
+++ readme.txt
@@ -0,0 +1,23 @@
+1) npm init
+
+2) add dependencies
+// "dependencies": {
+//   "bcryptjs": "*",                   //password hashing
+//   "body-parser": "*",               //JSON parser
+//   "connect-flash": "*",             //Flash messafes
+//   "cookie-parser": "^1.4.1",        //Cookies
+//   "express": "*",                   //
+//   "express-handlebars": "*",        //View engine (can use JADE)
+//   "express-messages": "*",          //Flash messaging
+//   "express-session": "*",           //Flash messaging
+//   "express-validator": "*",         //valiadte forms
+//   "mongodb":"*",                    //DB
+//   "mongoose": "*",                  //ORM for db
+//   "passport": "*",                  //
+//   "passport-http": "*",             //  
+//   "passport-local": "*"             //
+
+3) After adding these dependencies run - npm install
+
+
+//https://www.youtube.com/watch?v=OnuC3VtEQks - part 2
